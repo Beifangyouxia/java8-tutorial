e1ae13771b1efe76729607679aef6e43e6c37963
wenwan <aalansehaiyang52@126.com>
2019-07-04 19:47:42 +0800

    BiConsumer、BinaryOperator、Reduce

---
+++ src/com/winterbe/java8/samples/methodreference/BinaryOperator1.java
@@ -0,0 +1,34 @@
+package com.winterbe.java8.samples.methodreference;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+/**
+ * @author onlyone
+ */
+public class BinaryOperator1 {
+
+    public static void main(String[] args) {
+        int value1 = 12;
+        int value2 = 6;
+
+        // 最小值
+        System.out.println("最小值=" + getMin(value1, value2, (a, b) -> a - b));
+
+        // 最小值
+        System.out.println("最大值=" + getMax(value1, value2, (a, b) -> a - b));
+
+//         new ArrayList<List>().stream().mapToInt(List::size);
+    }
+
+    public static int getMin(int a, int b, Comparator<Integer> c) {
+        return BinaryOperator.minBy(c).apply(a, b);
+    }
+
+    public static int getMax(int a, int b, Comparator<Integer> c) {
+        return BinaryOperator.maxBy(c).apply(a, b);
+    }
+}
---
+++ src/com/winterbe/java8/samples/stream/Reduce1.java
@@ -0,0 +1,30 @@
+package com.winterbe.java8.samples.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author onlyone
+ */
+public class Reduce1 {
+
+    /**
+     * 求和
+     */
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // 方式一
+        int sum1 = list.stream().mapToInt(t -> t).sum();
+        System.out.println(sum1);
+
+        // 方式二
+        int sum2 = list.stream().mapToInt(t -> t).reduce(0, (x, y) -> x + y);
+        System.out.println(sum2);
+
+    }
+}
